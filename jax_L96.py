8ccee278c5ccad3a0bc8fcc3e46912f3f06c9e80
Nathan Schill <nateschill@gmail.com>
2024-11-12 22:33:01 -0700

    got jitted RK4 solver to work

--- jax_L96.py
+++ jax_L96.py
@@ -3,12 +3,14 @@ Code to run and nudge the L96 model using jax and jit.
 """
 
 from collections.abc import Callable
+from functools import partial
 
 import jax
 from jax import numpy as jnp, jit, lax
 # jax.config.update("jax_enable_x64", True)
 
-# Useful: jax.debug.print
+# import jax.debug
+# from jax.debug import print as jprint
 
 jndarray = jnp.ndarray
 
@@ -62,31 +64,61 @@ def ode(
     return u1, v1
 
 
-def rk4(
-    ode: Callable[[tuple, jndarray, jndarray], tuple[jndarray, jndarray]],
-    params: tuple,
-    u0: jndarray,
-    v0: jndarray,
-    t0: float,
-    tf: float,
-    dt: float,
-    # true_sol: None | Callable[[float], jndarray],
-) -> Callable[[float], jndarray]:
-    f = lambda u, v: ode(params, u, v)
-    I, J, *_ = params
+class RK4:
+    def __init__(
+        self,
+        ode: Callable[[tuple, jndarray, jndarray], tuple[jndarray, jndarray]],
+        params: tuple,
+    ):
+        self._params = params
 
-    tls = jnp.arange(t0, tf, dt)
-    N = len(tls)
+        def f(u, v):
+            return ode(params, u, v)
 
-    # Store the solution at every step.
-    U = jnp.full((N, I), jnp.inf)
-    V = jnp.full((N, I, J), jnp.inf)
+        self._f = f
 
-    # Set initial state.
-    U = U.at[0].set(u0)
-    V = V.at[0].set(v0)
+    def rk4(
+        self,
+        u0: jndarray,
+        v0: jndarray,
+        t0: float,
+        tf: float,
+        dt: float,
+    ) -> tuple[jndarray, jndarray]:
+        I, J = self._params[0], self._params[1]
 
-    for n in range(1, N):
+        tls = jnp.arange(t0, tf, dt)
+        N = len(tls)
+
+        return self._rk4(I, J, N, u0, v0, dt)
+
+    @partial(jit, static_argnames=("self", "I", "J", "N"))
+    def _rk4(
+        self,
+        I: int,
+        J: int,
+        N: int,
+        u0: jndarray,
+        v0: jndarray,
+        dt: float,
+    ) -> Callable[[float], jndarray]:
+        # Store the solution at every step.
+        U = jnp.full((N, I), jnp.inf)
+        V = jnp.full((N, I, J), jnp.inf)
+
+        # Set initial state.
+        U = U.at[0].set(u0)
+        V = V.at[0].set(v0)
+
+        (U, V), _ = lax.fori_loop(1, N, self._rk4_step, ((U, V), dt))
+
+        return U, V
+
+    def _rk4_step(self, n, vals):
+        f = self._f
+
+        (U, V), dt = vals
+
         u = U[n - 1]
         v = V[n - 1]
 
@@ -101,7 +133,7 @@ def rk4(
         U = U.at[n].set(u1)
         V = V.at[n].set(v1)
 
-    return U, V
+        return (U, V), dt
 
 
 def interpolate():
