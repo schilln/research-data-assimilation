e72f65d4b8f615b085974777391197559348b5ba
Nathan Schill <nateschill@gmail.com>
2025-02-13 10:46:41 -0700

    implemented method to compute mth spatial derivative of state

--- advection.py
+++ advection.py
@@ -2,7 +2,7 @@
 Advection equation u_t + c u_x = 0, x in [0, 2 Pi] with periodic boundary
 conditions.
 
-Test case for pseudospectral method.
+Test case for pseudo-spectral method.
 
 See ACME Volume 4 lab "Spectral 2: A Pseudospectral Method for Periodic
 Functions".
@@ -18,15 +18,27 @@ jndarray = jnp.ndarray
 
 class Advection(System):
     def ode(self, true: jndarray) -> jndarray:
-        u = true
-        n = len(u)
-        k = fft.rfftfreq(n)
-
-        return self.gs * fft.irfft(1j * k * fft.rfft(u))
+        return self.gs * Advection.d(true, 1)
 
     def estimated_ode(self, cs: jndarray, nudged: jndarray) -> jndarray:
-        v = nudged
-        n = len(v)
+        return cs * Advection.d(nudged, 1)
+
+    def d(s: jndarray, m: int) -> jndarray:
+        """Compute mth spatial derivative of the state.
+
+        Parameters
+        ----------
+        s
+            System state (e.g., true or nudged) at a point in time
+        m
+            Number of sptial derivatives to take
+
+        Returns
+        -------
+        d^m s / d {x^m}
+            Approximation of mth spatial derivative of s
+        """
+        n = len(s)
         k = fft.rfftfreq(n)
 
-        return cs * fft.irfft(1j * k * fft.rfft(v))
+        return fft.irfft((1j * k) ** m * fft.rfft(s))
