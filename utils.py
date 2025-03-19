aafe847af506da21a24c535f82349fa715d77654
Nathan Schill <nateschill@gmail.com>
2025-03-19 12:45:39 -0600

    updated some utils and plotting code

--- utils.py
+++ utils.py
@@ -27,7 +27,8 @@ def run_update(
     optimizer: Callable[[jndarray, jndarray], jndarray]
     | base_optim.Optimizer
     | None = None,
-) -> tuple[jndarray, np.ndarray, np.ndarray]:
+    return_all: bool = False,
+) -> tuple[jndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     """Use `solver` to run `system` and update parameter values with
     `optimizer`, and return sequence of parameter values and errors between
     nudged and true states.
@@ -60,6 +61,8 @@ def run_update(
         Note that an instance of `base_optim.Optimizer` implements this
         interface.
         If None, defaults to `base_optim.LevenbergMarquardt`.
+    return_all
+        If true, return true and nudged states for entire simulation.
 
     Returns
     -------
@@ -75,17 +78,41 @@ def run_update(
         The actual linspace of time values used, in multiples of `t_relax` from
         `T0` to approximately `Tf`
         shape (N + 1,) where N is the number of parameter updates performed
+    true
+        True states for final iteration of length `t_relax`,
+        or if `return_all` is True, then true states for entire simulation.
+    nudged
+        Nudged states for final iteration of length `t_relax`,
+        or if `return_all` is True, then nudged states for entire simulation.
     """
     if optimizer is None:
         optimizer = base_optim.LevenbergMarquardt(system)
 
     if isinstance(solver, base_solver.SinglestepSolver):
         return _run_update_singlestep(
-            system, solver, dt, T0, Tf, t_relax, true0, nudged0, optimizer
+            system,
+            solver,
+            dt,
+            T0,
+            Tf,
+            t_relax,
+            true0,
+            nudged0,
+            optimizer,
+            return_all,
         )
     elif isinstance(solver, base_solver.MultistepSolver):
         return _run_update_multistep(
-            system, solver, dt, T0, Tf, t_relax, true0, nudged0, optimizer
+            system,
+            solver,
+            dt,
+            T0,
+            Tf,
+            t_relax,
+            true0,
+            nudged0,
+            optimizer,
+            return_all,
         )
     else:
         raise NotImplementedError(
@@ -106,7 +133,8 @@ def _run_update_singlestep(
     optimizer: Callable[[jndarray, jndarray], jndarray]
     | base_optim.Optimizer
     | None = None,
-) -> tuple[jndarray, np.ndarray, np.ndarray]:
+    return_all: bool = False,
+) -> tuple[jndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     """Implementation of `run_update` for non-multistep solvers (e.g., RK4),
     here referred to as 'singlestep' solvers. See documentation of `run_update`.
     """
@@ -117,6 +145,12 @@ def _run_update_singlestep(
 
     cs = [system.cs]
     errors = []
+
+    if return_all:
+        trues, nudgeds = (
+            [np.expand_dims(true0, 0)],
+            [np.expand_dims(nudged0, 0)],
+        )
 
     t0 = T0
     tf = t0 + t_relax
@@ -137,12 +171,22 @@ def _run_update_singlestep(
             np.linalg.norm(true[1:] - nudged[1:]) / np.linalg.norm(true[1:])
         )
 
+        if return_all:
+            trues.append(true[1:])
+            nudgeds.append(nudged[1:])
+
     errors = np.array(errors)
 
     # Note the last `t0` is the actual final time of the simulation.
     tls = np.linspace(T0, t0, len(errors) + 1)
 
-    return jnp.stack(cs), errors, tls
+    return (
+        jnp.stack(cs),
+        errors,
+        tls,
+        np.concatenate(trues) if return_all else true,
+        np.concatenate(nudgeds) if return_all else nudged,
+    )
 
 
 def _run_update_multistep(
@@ -157,11 +201,15 @@ def _run_update_multistep(
     optimizer: Callable[[jndarray, jndarray], jndarray]
     | base_optim.Optimizer
     | None = None,
-) -> tuple[jndarray, np.ndarray, np.ndarray]:
+    return_all: bool = False,
+) -> tuple[jndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     """Implementation of `run_update` for multistep solvers (e.g.,
     Adams–Bashforth). See documentation of `run_update`.
     """
     assert isinstance(solver, base_solver.MultistepSolver)
+
+    if return_all:
+        raise NotImplementedError("`return_all` not implemented yet")
 
     if optimizer is None:
         optimizer = base_optim.LevenbergMarquardt(system)
@@ -219,4 +267,4 @@ def _run_update_multistep(
     # Note the last `t0` is the actual final time of the simulation.
     tls = np.linspace(T0, t0, len(errors) + 1)
 
-    return jnp.stack(cs), errors, tls
+    return jnp.stack(cs), errors, tls, true, nudged
