94ee76039e5c30dcea55df44fdbc39d0fe805dc8
Nathan Schill <nateschill@gmail.com>
2025-04-22 15:01:35 -0600

    experimented with 1D wave eqn solved in Fourier domain

--- examples/wave_1d_fourier.py
+++ examples/wave_1d_fourier.py
@@ -8,6 +8,9 @@ See ACME Volume 4 lab "Spectral 2: A Pseudospectral Method for Periodic
 Functions".
 """
 
+from functools import partial
+
+import jax
 from jax import numpy as jnp
 from jax.numpy import fft
 
@@ -26,10 +29,12 @@ class Wave(System):
         observed_slice: slice,
         x0: float,
         xf: float,
+        xn: int,
     ):
         super().__init__(μ, gs, bs, cs, observed_slice)
 
         self._period = xf - x0
+        self.xn = xn
 
     def ode(self, true: jndarray) -> jndarray:
         u, ut = true
@@ -54,7 +59,15 @@ class Wave(System):
         d^m s / d {x^m}
             Approximation of mth spatial derivative of s
         """
-        n = len(s)
-        k = fft.rfftfreq(n, self._period / n)
+        k = fft.rfftfreq(self.xn, self._period / self.xn)
 
-        return fft.irfft((2 * jnp.pi * 1j * k) ** m * fft.rfft(s))
+        return (2 * jnp.pi * 1j * k) ** m * s
+
+    @partial(jax.jit, static_argnames="self")
+    def _compute_w(self, cs: jndarray, nudged: jndarray) -> jndarray:
+        return (
+            jax.jacrev(self.estimated_ode, 0, holomorphic=True)(cs, nudged)[
+                self.observed_slice
+            ].T
+            / self.μ
+        )
